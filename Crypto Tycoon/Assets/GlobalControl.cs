b31b83800e72faf77efea45a1c85c98a5cb21608
OriolGallego2001 <oriol.gallego01@estudiant.upf.edu>
2022-02-24 18:16:06 +0100

    DateImplementacion
    Hay una fecha que varia con el tiempo si el juego no esta en pause y el dinero varia tambien con el tiempo y en un futuro el numero de ordenadores.

--- Crypto Tycoon/Assets/GlobalControl.cs
+++ Crypto Tycoon/Assets/GlobalControl.cs
@@ -1,21 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+
 
 public class GlobalControl : MonoBehaviour
 {
-    public int PCnum;
+    private double TimeStart;
+    public Text timeText;
+    static DateTime myDate;
+
+
+    public bool isPaused;
+    public Image pauseimg;
+    public Sprite pauseSprite;
+    public Sprite playSprite;
+
+
+    public double PCnum;
+    public Text moneyText;
+    public double money;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        myDate = new DateTime (2022,2,24);
+        isPaused = false;
         PCnum = 2;
+        money = 3000;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isPaused)
+        {
+            TimeStart += Time.deltaTime;
+            money += Time.deltaTime * (PCnum * 10);
+        }
         
+        if (TimeStart > 2.0)
+        {
+            myDate=myDate.AddDays(1);
+            TimeStart = 0.0;
+        }
+        timeText.text = myDate.ToString();
+        moneyText.text = Math.Ceiling(money).ToString();
+    }
+
+
+    public void pauseUnpause()
+    {
+        isPaused = !isPaused;
+        if (pauseimg.sprite.name == "GUI_34")
+        {
+            pauseimg.sprite = playSprite;
+            //pause the time
+        }
+        else
+        {
+            pauseimg.sprite = pauseSprite;
+            //resume the time
+        }
     }
 }
